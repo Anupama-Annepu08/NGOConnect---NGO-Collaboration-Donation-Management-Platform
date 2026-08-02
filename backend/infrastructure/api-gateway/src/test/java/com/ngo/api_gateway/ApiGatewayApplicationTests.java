7dcf00c798de14f20f78553b8c06222802b56bf4
Anupama-Annepu08 <anupamaannepu8@gmail.com>
2026-08-02 17:45:37 +0530

    Add API Gateway with Eureka registration

---
+++ backend/infrastructure/api-gateway/src/test/java/com/ngo/api_gateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ngo.api_gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
